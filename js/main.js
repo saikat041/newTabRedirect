ffd13267b1219bcc2d7bdf5138363bf046e2882d
Saikat Sarkar <saikat.dakshana15@gmail.com>
2019-06-09 11:46:16 +0530

    Added Time Feature

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 var noOfImages = 25;
 
 var noOfQuotes = 5400;
@@ -13,3 +12,36 @@ $.getJSON('res/quotes.json').done(function(data){
     document.getElementById('quote').innerText = data[randomQuoteNumber].quoteText;
     document.getElementById('author').innerText = data[randomQuoteNumber].quoteAuthor;
 })
+
+// Padding integer with 0 in the begining
+function pad(s){
+    s = String(s)
+    if (s.length == 1){
+        s = '0' + s
+    }
+    return s
+}
+
+function getCurrentTime(){
+
+am_pm = 'AM'
+date = new Date
+
+h = date.getHours()
+m = date.getMinutes()
+s = date.getSeconds()
+
+if(h>12){
+    h = h%12
+    am_pm = 'PM'
+}
+
+time = pad(h) + ':' + pad(m) + ':' + pad(s) + ' ' + am_pm
+
+return time
+
+}
+
+setInterval(() => {
+document.getElementById('time').innerText = getCurrentTime()
+}, 1);
